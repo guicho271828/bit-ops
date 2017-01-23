c3db43b61cead9e9248f8f7db096cb5823768a30
Masataro Asai <guicho2.71828@gmail.com>
2017-01-24 02:26:47 +0900

    implemented register allocation optimization

--- src/package.lisp
+++ src/package.lisp
@@ -89,9 +89,13 @@ into (bit-not subform <temporary storage>) .
   (assert (= (length body) 1))
   (compile-bitwise-operations (first body)))
 
+(defvar *register-allocation-optimization* t)
+(defvar *common-subexpression-elimination* t)
+
 (defstruct op
-  form
-  storage)
+  name
+  inputs
+  output)
 
 (defvar *ops* nil)
 (defvar *first-variable* nil)
@@ -99,55 +103,85 @@ into (bit-not subform <temporary storage>) .
   (let ((*ops* nil)
         (*first-variable* nil))
     (let ((result-symbol (parse-form form)))
-      (with-gensyms (len)
-        `(let* ((,len (length ,*first-variable*))
-                (,result-symbol (make-bit-vector ,len)))
-           (dlet* ((+zero+ (make-bit-vector ,len))
-                   (+one+  (make-bit-vector ,len))
-                   ,@(mapcar (lambda (op)
-                               `(,(op-storage op) (make-bit-vector ,len)))
-                             (rest      ; since the first op should be the result-symbol
-                              *ops*)))
-             (declare (ignorable +zero+ +one+))
-             ,@(mapcar (lambda-ematch
-                         ((op form storage)
-                          `(,@form ,storage))) (reverse *ops*))
-             ,result-symbol))))))
+      (setf *ops* (nreverse *ops*))     ;in order
+      (when *register-allocation-optimization*
+        (reduce-allocation))
+      (build-forms result-symbol))))
+
+(defun reduce-allocation ()
+  (iter (for op1 in *ops*)
+        (ematch op1
+          ((op :name n1 :inputs i1 :output o1)
+           (let ((successors
+                  (iter (for op2 in *ops*)
+                        (when (member o1 (op-inputs op2))
+                          (collect op2)))))
+             (match successors
+               ((list (and op2 (op :name n2 :inputs i2 :output o2)))
+                ;; only 1 op depends on op; share storage
+                (print `(:removing ,o1))
+                (setf *ops* (substitute (op n1 i1 o2) op1 *ops*))
+                (setf *ops* (substitute (op n2 (substitute o2 o1 i2) o2)
+                                        op2 *ops*)))))))))
+
+(defun build-forms (result-symbol)
+  (with-gensyms (len)
+    `(let* ((,len (length ,*first-variable*))
+            (,result-symbol (make-bit-vector ,len)))
+       (dlet* ((+zero+ (make-bit-vector ,len))
+               (+one+  (make-bit-vector ,len))
+               ,@(mapcar (lambda (out)
+                           `(,out (make-bit-vector ,len)))
+                         (remove-duplicates
+                          (remove result-symbol
+                                  (mapcar #'op-output *ops*)))))
+         (declare (ignorable +zero+ +one+))
+         ,@(mapcar (lambda-ematch
+                     ((op name inputs output)
+                      `(,name ,@inputs ,output))) *ops*)
+         ,result-symbol))))
+
+(defun-match* common-subexpression (op1 op2)
+  (((op name inputs)
+    (op :name (eq name) :inputs (equal inputs)))
+   t))
+
+(defun accumulate (op)
+  ;; it is not ok to remove-duplicate in post-processing since
+  ;; subsequent call to the same expression should be rewritten.
+  (if-let ((op2 (and *common-subexpression-elimination*
+                     (find op *ops* :test #'common-subexpression))))
+    (op-output op2)
+    (progn
+      (push op *ops*)
+      (op-output op))))
 
 (defun parse-form (form)
-  (flet ((accumulate (op)
-           (if-let ((op2 (find op *ops* :key #'op-form :test #'equalp)))
-             (op-storage op2)
-             (progn
-               (push op *ops*)
-               (op-storage op)))))
-    (ematch form
-      (0 '+zero+)
-      (1 '+one+)
-      ((symbol)
-       (setf *first-variable* form))
-      ((list 'not arg)
-       (accumulate (op `(bit-not ,(parse-form arg)) (gensym))))
-      ((list (and binary-op
-                  (or 'andc1 'andc2 'orc1 'orc2))
-             arg1 arg2)
-       (accumulate (op `(,(symbolicate 'bit- binary-op)
-                          ,(parse-form arg1)
-                          ,(parse-form arg2)) (gensym))))
-      ((list (and commutative-op
-                  (or 'and 'eqv 'ior 'nand 'nor 'xor))
-             arg)
-       (parse-form arg))
-      ((list* (and commutative-op
-                   (or 'and 'eqv 'ior 'nand 'nor 'xor))
-              arg args)
-       (accumulate (op `(,(symbolicate 'bit- commutative-op)
-                          ,(parse-form arg)
-                          ,(parse-form `(,commutative-op ,@args))) (gensym))))
-      ((list* (and macro-op (symbol)) args)
-       (parse-form
-        (funcall (symbol-bitwise-operation macro-op)
-                 args))))))
+  (ematch form
+    (0 '+zero+)
+    (1 '+one+)
+    ((symbol)
+     (setf *first-variable* form))
+    ((list 'not arg)
+     (accumulate (op 'bit-not (list (parse-form arg)) (gensym))))
+    ((list (and binary-op
+                (or 'andc1 'andc2 'orc1 'orc2))
+           arg1 arg2)
+     (accumulate (op (symbolicate 'bit- binary-op)
+                     (list (parse-form arg1)
+                           (parse-form arg2)) (gensym))))
+    ((list (or 'and 'eqv 'ior 'nand 'nor 'xor) arg)
+     (parse-form arg))
+    ((list* (and commutative-op
+                 (or 'and 'eqv 'ior 'nand 'nor 'xor))
+            arg args)
+     (accumulate (op (symbolicate 'bit- commutative-op)
+                     (list (parse-form arg)
+                           (parse-form `(,commutative-op ,@args))) (gensym))))
+    ((list* (and macro-op (symbol)) args)
+     (parse-form
+      (funcall (symbol-bitwise-operation macro-op)
+               args)))))
 
 (as-bitwise-operations ()
   (and a b))
@@ -155,6 +189,10 @@ into (bit-not subform <temporary storage>) .
 (as-bitwise-operations ()
   (and a b c))
 
+(as-bitwise-operations ()
+  (and (and a b)
+       (and a b)
+       c))
 
 ;; 
 ;; ->
