e11aecfa1c829d7fa8ce199e7bef225897f9881f
Masataro Asai <guicho2.71828@gmail.com>
2017-01-24 04:07:36 +0900

    adder

--- src/macros.lisp
+++ src/macros.lisp
@@ -9,3 +9,19 @@
 (define-bitwise-operation implies (lhs rhs)
   `(ior (not ,lhs)
         ,rhs))
+
+
+(define-bitwise-operation half-adder-sum (a b)
+  `(xor ,a ,b))
+(define-bitwise-operation half-adder-carry (a b)
+  `(and ,a ,b))
+
+(define-bitwise-operation full-adder-sum (a b x)
+  `(half-adder-sum (half-adder-sum ,a ,b)
+                   ,x))
+
+(define-bitwise-operation full-adder-carry (a b x)
+  `(ior (half-adder-carry ,a ,b)
+        (half-adder-carry (half-adder-sum ,a ,b)
+                          ,x)))
+
