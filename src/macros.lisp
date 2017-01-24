f706a260bffee90d069d2c00847e72b568020c89
Masataro Asai <guicho2.71828@gmail.com>
2017-01-24 10:27:44 +0900

    doc

--- src/macros.lisp
+++ src/macros.lisp
@@ -16,24 +16,30 @@ Take the disjunction of arguments and invert the results."
 
 
 (define-bitwise-operation if (condition then else)
+  "Take THEN bit when CONDITION bit is true, take ELSE otherwise."
   `(ior (and ,condition ,then)
         (andc1 ,condition ,else)))
 
 (define-bitwise-operation implies (lhs rhs)
+  "LHS => RHS :- (or (not LHS) RHS)"
   `(ior (not ,lhs)
         ,rhs))
 
 
 (define-bitwise-operation half-adder-sum (a b)
+  "sum output of half adder "
   `(xor ,a ,b))
 (define-bitwise-operation half-adder-carry (a b)
+  "carry output of half adder "
   `(and ,a ,b))
 
 (define-bitwise-operation full-adder-sum (a b x)
+  "sum output of full adder "
   `(half-adder-sum (half-adder-sum ,a ,b)
                    ,x))
 
 (define-bitwise-operation full-adder-carry (a b x)
+  "carry output of full adder "
   `(ior (half-adder-carry ,a ,b)
         (half-adder-carry (half-adder-sum ,a ,b)
                           ,x)))
